531179f37d1810c9f29e99114c72754df5a3693f
leilamyar <leilamyar.dev@gmail.com>
2021-10-27 20:15:23 +0200

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 const routes: Routes = [];
 
